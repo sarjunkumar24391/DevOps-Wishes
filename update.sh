9889be97b26f105b2941473bcd1f7f0ff1315d3c
sarjunkumar24391 <arjunkumar.s@cognizant.com>
2016-04-04 10:31:50 +0530

    Update update.sh
    removing --rm from line 8

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
 docker build -f Dockerfile.build -t hello-world:build .
-docker run --rm hello-world:build cat hello > hello
+docker run hello-world:build cat hello > hello
 chmod +x hello
